942343548650a0afcb3c9c0bf5e78371080db8d2
ryutaroM <arcticmonkey326@gmail.com>
2026-01-21 23:54:44 +0900

    chore: add book repository trait

---
+++ kernel/src/repository/book.rs
@@ -0,0 +1,11 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use uuid::Uuid;
+
+use crate::model::book::{Book, event::CreateBook};
+#[async_trait]
+pub trait BookRepository: Send + Sync {
+    async fn create(&self, event: CreateBook) -> Result<()>;
+    async fn find_all(&self) -> Result<Vec<Book>>;
+    async fn find_by_id(&self, book_id: Uuid) -> Result<Option<Book>>;
+}
