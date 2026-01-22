e989141f8fb98c5d77ceb0c4d9269264e4425bb0
ryutaroM <arcticmonkey326@gmail.com>
2026-01-22 23:53:46 +0900

    chore: implement create method in BookRepositoryImpl

--- adapter/src/repository/book.rs
+++ adapter/src/repository/book.rs
@@ -15,14 +15,27 @@ pub struct BookRepositoryImpl {
 #[async_trait]
 impl BookRepository for BookRepositoryImpl {
     async fn create(&self, event: CreateBook) -> Result<()> {
-        todo!()
+        sqlx::query!(
+            r#"
+                INSERT INTO books (title, author, isbn, description)
+                VALUES ($1, $2, $3, $4)
+            "#,
+            event.title,
+            event.author,
+            event.isbn,
+            event.description
+        )
+        .execute(self.db.inner_self())
+        .await?;
+
+        Ok(())
     }
 
     async fn find_all(&self) -> Result<Vec<Book>> {
         todo!()
     }
 
-    async fn find_by_id(&self, book_id: Uuid) -> Result<Option<Book>> {
+    async fn find_by_id(&self, _book_id: Uuid) -> Result<Option<Book>> {
         todo!()
     }
 }
