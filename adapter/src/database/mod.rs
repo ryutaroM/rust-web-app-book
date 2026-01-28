7aa13b7d357d8fcf4ed6ce17c91577315d724c8d
ryutaroM <arcticmonkey326@gmail.com>
2026-01-28 23:45:21 +0900

    chore: add test for register books and add new func

--- adapter/src/database/mod.rs
+++ adapter/src/database/mod.rs
@@ -1,5 +1,5 @@
 use shared::config::DatabaseConfig;
-use sqlx::{PgPool, postgres::PgConnectOptions};
+use sqlx::{PgPool, pool, postgres::PgConnectOptions};
 pub mod model;
 
 fn make_pg_connect_options(cfg: &DatabaseConfig) -> PgConnectOptions {
@@ -15,6 +15,10 @@ fn make_pg_connect_options(cfg: &DatabaseConfig) -> PgConnectOptions {
 pub struct ConnectionPool(PgPool);
 
 impl ConnectionPool {
+    pub fn new(pool: PgPool) -> Self {
+        Self(pool)
+    }
+
     pub fn inner_ref(&self) -> &PgPool {
         &self.0
     }
