ec70d9c66b4c877997eb4aebcb9b91497bc8ec0a
ryutaroM <arcticmonkey326@gmail.com>
2026-04-02 21:39:11 +0900

    chore: remove old error definition

--- api/src/handler/book.rs
+++ api/src/handler/book.rs
@@ -1,28 +1,16 @@
 use std::any;
 
 use axum::{
-    Json,
     extract::{Path, State},
     http::StatusCode,
     response::{IntoResponse, Response},
+    Json,
 };
 use registry::AppRegistry;
 use thiserror::Error;
 use uuid::Uuid;
 
 use crate::model::book::{BookResponse, CreateBookRequest};
-
-#[derive(Error, Debug)]
-pub enum AppError {
-    #[error("{0}")]
-    InternalError(#[from] anyhow::Error),
-}
-
-impl IntoResponse for AppError {
-    fn into_response(self) -> Response {
-        (StatusCode::INTERNAL_SERVER_ERROR, "").into_response()
-    }
-}
 
 pub async fn register_book(
     State(registry): State<AppRegistry>,
